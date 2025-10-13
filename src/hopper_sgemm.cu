8d497e3086eb5738272c610a83a101acbefd9da7
xianghao <wang.xianghao@outlook.com>
2025-10-13 22:38:09 +0800

    add installation configuration and testing app

--- src/hopper_sgemm.cu
+++ src/hopper_sgemm.cu
@@ -10,4 +10,8 @@ __global__ void hopper_sgemm_kernel(int m, int n, int k, float alpha,
 void hopper_sgemm(int m, int n, int k, float alpha, const float* A, int lda,
                   const float* B, int ldb, float* C, int ldc)
 {
+    dim3 blockDim(32U * 32U, 1U);
+    dim3 gridDim(128U, 128U, 1U);
+    hopper_sgemm_kernel<<<gridDim, blockDim>>>(m, n, k, alpha, A, lda, B, ldb,
+                                               C, ldc);
 }
--- test_sgemm.cpp
+++ test_sgemm.cpp
@@ -1,3 +1,8 @@
 #include <hopper_sgemm.hpp>
 
-int main() { return 0; }
+int main()
+{
+    hopper_sgemm(0, 0, 0, 0, nullptr, 0, nullptr, 0, nullptr, 0);
+
+    return 0;
+}
