efa69cae2c336989252ca102e13f95d313816dea
xianghao <wang.xianghao@outlook.com>
2025-10-15 18:33:45 +0800

    update profile.sh

--- profile.sh
+++ profile.sh
@@ -1,3 +1,6 @@
 target=./build/gemm_fp32_app
 
-ncu -f -o gemm_fp32_app --import-source yes $target
+ncu -f -o gemm_fp32_app --import-source yes \
+    --set full \
+    --kernel-id ::hopper_gemm_fp32_kernel:2 \
+    $target
